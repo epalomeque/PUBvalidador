aedc61f83ad9d85aa009c0503475fc4559a28e5d
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2016-12-08 19:48:04 -0600

    formatos completos - errores detectados... nuevo modo de validar con listas y diccionarios-backupdb completo

--- consultaCatalogos/templatetags/consulta_extras.py
+++ consultaCatalogos/templatetags/consulta_extras.py
@@ -1,3 +1,24 @@
 from django import template
 
+
 register = template.Library()
+
+
+def get_valor(value, arg):
+    dicc = value.get(arg)
+    return dicc.get('valor')
+
+
+def get_esvalido(value, arg):
+    dicc = value.get(arg)
+    return dicc.get('esvalido')
+
+
+def get_descrip(value, arg):
+    dicc = value.get(arg)
+    return dicc.get('descrip')
+
+
+register.filter('get_valor', get_valor)
+register.filter('get_esvalido', get_esvalido)
+register.filter('get_descrip', get_descrip)
