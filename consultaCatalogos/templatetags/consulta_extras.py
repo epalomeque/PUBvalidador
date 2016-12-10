47c2d8b19d80ff88924f7c19afbfe96b1d584568
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2016-12-09 18:30:58 -0600

    formatos completos - errores detectados... nuevo modo de validar con listas y diccionarios-backupdb completo

--- consultaCatalogos/templatetags/consulta_extras.py
+++ consultaCatalogos/templatetags/consulta_extras.py
@@ -3,7 +3,7 @@ from django import template
 
 register = template.Library()
 
-
+# Filtros para el registro de un valor
 def get_valor(value, arg):
     dicc = value.get(arg)
     return dicc.get('valor')
@@ -19,6 +19,23 @@ def get_descrip(value, arg):
     return dicc.get('descrip')
 
 
+def siselectedno(value, arg):
+    dicc = value.get(arg)
+    if dicc.get('valor') == 'NO':
+        return 'selected=selected'
+    else:
+        return ''
+
+
+def siselectedsi(value, arg):
+    dicc = value.get(arg)
+    if dicc.get('valor') == 'SI':
+        return 'selected=selected'
+    else:
+        return ''
+
 register.filter('get_valor', get_valor)
 register.filter('get_esvalido', get_esvalido)
 register.filter('get_descrip', get_descrip)
+register.filter('siselectedno', siselectedno)
+register.filter('siselectedsi', siselectedsi)
